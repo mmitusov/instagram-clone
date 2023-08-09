ceeadd56d5309eaa36c37ddc60984bad01ed37d8
mmitusov <maksym.mitusov@gmail.com>
2023-08-10 00:08:41 +0300

    NextAuth + Firebase

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,9 +1,28 @@
+import { getProviders, signIn } from 'next-auth/react'
 import React from 'react'
 
-const signIn = () => {
+const signInPage = ({ providers }: any) => {
+
   return (
-    <div>signIn</div>
+    <>
+      {/* Looping trough list of providers that we created in [...nextauth].ts */}
+      {Object.values(providers).map((provider: any) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name} {/* Dynamicly displaing names of the providers */}
+          </button>
+        </div>
+      ))}
+    </>
   )
 }
 
-export default signIn
+export default signInPage
+
+export async function getServerSideProps() {
+  const providers: any = await getProviders();
+  
+  return {
+    props: { providers },
+  }
+}
