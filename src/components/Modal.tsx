f132947c13d9525fae1bfb867899b9e3e3784610
mmitusov <maksym.mitusov@gmail.com>
2023-08-11 14:58:06 +0300

    Modal window

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,24 @@
 import { modalState } from '@/store/modalAtom';
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import { useRecoilState } from 'recoil';
 import { Transition, Dialog } from '@headlessui/react';
+import { CameraIcon } from '@heroicons/react/24/outline'
+
 
 const Modal = () => {
   const [isModalOpen, setIsModalOpen] = useRecoilState(modalState);
+  const [selectegImg, setSelectegImg] = useState<string>('')
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+  
+  const uploadPost = () => {
+    // console.log('sasdasdas')
+  }
+
+  const addImageToPost = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setSelectegImg(URL.createObjectURL(e.target.files[0]))
+  }
 
   return (
     <Transition.Root show={isModalOpen} as={Fragment }>
@@ -14,6 +28,7 @@ const Modal = () => {
         onClose={() => setIsModalOpen(false) as any}
       >
         <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+          {/* Overlay */}
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -34,6 +49,7 @@ const Modal = () => {
             &#8203;
           </span>
 
+          {/* Modal's Content */}
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -47,7 +63,69 @@ const Modal = () => {
               inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden
               shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6
             ">
-              Hello
+              <div>
+                {
+                  selectegImg 
+                    ?
+                      <img 
+                        src={selectegImg} 
+                        alt='imgToUpload' 
+                        onClick={() => setSelectegImg('')}
+                        className='w-full object-contain cursor-pointer'
+                      />
+                    :
+                      <div
+                        onClick={() => {filePickerRef?.current?.click()}}
+                        className='mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer'
+                      >
+                        <CameraIcon 
+                          className='w-6 h-6 text-red-600'
+                          aria-hidden='true'
+                        />
+                      </div>
+                }
+                <input
+                  ref={filePickerRef}
+                  onChange={addImageToPost}
+                  type="file"
+                  hidden
+                />
+
+                <div>
+                  <div className='mt-3 text-center sm:mt-5'>
+                    <Dialog.Title
+                      as='h3'
+                      className='text-lg leading-6 font-medium text-gray-900'
+                    >
+                      Upload a photo
+                    </Dialog.Title>
+                    <div>
+                      <input
+                        className='border-none focus:ring-0 w-full text-center'
+                        ref={captionRef}
+                        type="text"
+                        placeholder='Please enter a caption...'
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className='mt-5 sm:mt-6'>
+                  <button
+                    type='button'
+                    disabled={!selectegImg}
+                    onClick={uploadPost}
+                    className='
+                      inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4
+                      py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
+                      focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300
+                      disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    '
+                  >
+                    Upload Post
+                  </button>
+                </div>
+              </div>
             </div>
           </Transition.Child>
         </div>
